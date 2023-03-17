c1edc68c03f192d3f7bdaf7038d236d0b9b201b8
Mitchell Finlay <mitchell.finlay00@gmail.com>
2023-03-17 21:16:37 +1100

    done with assignment

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
   }
   
-function mondayWork(activity = "go to the office") {
+const mondayWork = function(activity = "go to the office") {
     return `This Monday, I will ${activity}.`;
-  }
-  
+  };
+
+
 function wrapAdjective(flair = "*") {
     return function (adjective = "special") {
       return `You are ${flair}${adjective}${flair}!`;
